9731b558f5a3c89462f2872be80dfea5c13fdbf1
Ashley-L <ashley.lall@outlook.com>
2020-02-05 13:23:38 -0500

    Done.

--- js/main.js
+++ js/main.js
@@ -8,19 +8,23 @@ let modalWindow = document.querySelector('#modal')
 let signIn = document.querySelector('#signin')
 let closeButton = document.querySelector(`#close`)
 let submitButton = document.querySelector('#submit')
+
 let inputField = document.querySelectorAll(`.field`)
-let userInputs = document.querySelectorAll("input")
-let form = document.querySelector('#getstarted')
+var userInputs = document.querySelectorAll("input")
 let inputUsername = document.getElementById('user')
 let inputPass = document.getElementById('pass')
+// let userValue = inputUsername.value.trim();
+// let passValue = inputPass.value.trim();
+
 let greeting = document.querySelector('#hello');
+let form = document.querySelector('#getstarted')
+
+
 
 
 
 
 // Display modal window when you click sign in
-
-
 signIn.addEventListener(`click`,(event) => {
     modalWindow.style.display = `block`;
 })
@@ -36,7 +40,7 @@ closeButton.addEventListener(`click`,(event) => {
 // Show error when you click submit for BOTH user and password
 
 
-    // shoe error when you click for both, but individually
+    // show error when you click for both, but individually
     // submitButton.addEventListener(`click`,(event) => {
     //     // document.querySelector('#user').classList.add(`error`);
     //     // document.querySelector('#pass').classList.add(`error`);
@@ -69,6 +73,7 @@ closeButton.addEventListener(`click`,(event) => {
 
 userInputs.forEach(field => {
     field.addEventListener(`focus`, (event) => {
+        console.log(`there's a focus`)
         //document.getElementsByTagName(`input`).classList.remove(`error`)
         console.log(event);
         event.target.classList.remove(`error`) 
@@ -76,25 +81,51 @@ userInputs.forEach(field => {
 })
 
 
-// To find what is being targeted, go console > click on whatever shit on the website > look at the dropdown > go to target////////////////////////////////////////////////  
+
+
+// To find what is being targeted
+// console.log(event)
+// go console > click on whatever shit on the website 
+// > look at the dropdown > go to target
 
 
 
-
+//////////////////////////////////////////////////////
 ///////////////////////PART 2/////////////////////////
+//////////////////////////////////////////////////////
 // FUNCTION: WHEN YOU CLICK SUBMIT
-// userInputs.addEventListener(`change`, (event) => {
-//     if (event.target.value === "") {
-//         userInputs.target.classList.add(`error`)
-//     }
+
+
+
+// userInputs.forEach(input => {
+//     console.log(`there are two fields`)
+//     input.addEventListener(`blur`, (event) => {
+//         console.log(`there's a blur`)
+
+//         // let userValue = inputUsername.value.trim();
+//         // if (userValue === "") {
+//         //     event.target.classList.add(`error`)
+//         //     console.log(`username is invalid`)
+//         // } else {console.log(`username is valid`)}
+
+//         let passValue = inputPass.value.trim();
+//         if (passValue === "") {
+//             event.target.classList.add(`error`)
+//             console.log(`password is invalid`)
+//         } else {console.log(`password is valid`)}
+        
+//         // if (userValue === "" && passValue === "") {
+//         //     event.target.classList.add(`error`)
+//         //     // console.log(`password is invalid`)
+//         // } 
+//     })
 // })
 
-// let greetingUser = greetinWelcome.textContent;
-
-// let greeting = document.querySelector('#hello');
 
 
 
+
+// THIS EVENT LISTENER IS ON SUBMIT
 
 // STEP 1. Prevent from submitting when submit is clicked
 form.addEventListener('submit', event => {
@@ -105,19 +136,26 @@ form.addEventListener('submit', event => {
     // Trim whitespace so typing in spaces will still show an error
 
     // Error for username w/ value ""
-   
     let userValue = inputUsername.value.trim();
     console.log(`The username is ${userValue}`);
     if (userValue === "") {
         inputUsername.classList.add(`error`)
     }
    
-    // Error for password w/ value ""
+    // // Error for password w/ value ""
     let passValue = inputPass.value.trim();
     console.log(`The password is ${passValue}`)
     if (passValue === "") {
         inputPass.classList.add(`error`)
     }
+
+   
+
+
+
+
+
+    
 
     // STEP 3. If both fields valid on submit
         // close modal
@@ -147,16 +185,66 @@ form.addEventListener('submit', event => {
     }
 })
 
-// submitButton.addEventListener(`click`, (event) => {
+//////////////////////////////////////////////////////////
+// STEP 4. Error after you remove cursor from the field //
+//////////////////////////////////////////////////////////
 
-    
-// })
+
+// 1. FIRST YOU NEED TO GET THE INPUTS, WHICH YOU ALREADY DID
+    // var userInputs = document.querySelectorAll("input")
+
+// 2. You are going to test if an input's value is false
+    // So make a variable and set it to false
+    // Empty strings ("") are false by default
+    var emptyValue = false;
+
+// 3. Write a function (outline) to validate the inputs
+    // Parameters can also be any name, stop forgetting
+function validateInputs (thetwoInputs) {
+
+   
+    // 4. running the function FOR EACH input (individually)
+    thetwoInputs.forEach(input => {
+
+        // 5. Add the event listener
+        //  listening for a blur on (each) input
+        input.addEventListener(`blur`, event => {
+            // if the value of an input is NOT false/empty, add .error
+            // you have to do 'if not empty' first
+            if (input.value != emptyValue) {
+                input.classList.remove(`error`);
+            } else { //if it IS empty, add the .error
+                input.classList.add(`error`)
+            }
+        })
+    })
+}
+
+// This whole thing basically says
+// for each input, look for a blur
+// if the string is not empty, remove the error class
+// and if it's still empty, add the error class
+
+// run the function on userInputs (qhich is querySelectorAll on inputs)
+validateInputs(userInputs);
 
 
-// STEP 4. 
-// input.addEventListener(`change`, (event) => {
-    
-// })
+
+// STEP 5. Click the modal to close
+modalWindow.addEventListener(`click`, event => {
+    modalWindow.style.display = `none`;
+    console.log(event);
+})
+// Parent: #modal --> modalWindow
+// Child: #getstarted --> form
+// Problem: clicking the child (form) triggers the eventlistener (close) on the parent (modalWindow)
+// Solution: add stopPropagation to the child to stop the click from bubbling up
+
+form.addEventListener(`click`, event => {
+    event.stopPropagation();
+})
+
+
 
 
 
@@ -186,3 +274,98 @@ FOR TAKE-HOME
 - only need height values really
 - need css
 */
+
+
+
+
+
+
+
+
+// WRITE YOUR STEPS HOLY SHIT
+// step 4.4 for each #input??? or field???
+// userInputs.forEach(input => {
+    // step 4.3. blur (opposite of focus) on the field (b/c it's the target)
+//     inputUsername.addEventListener(`blur`, event => { 
+//         console.log(event)
+//         // step 4.2. if statement: if value === "" then add .error
+//         if (userValue === "") {
+//             // step 4.1. add .error to #input
+//             event.target.classList.add(`error`)
+//             // console.log(`username is ${userValue}, password is ${passValue}`)
+//         }
+//     }) 
+// // })
+
+
+// var on input, but maybe not querySelector all
+
+
+// var inputs = document.value.trim()
+// userInputs.forEach(field => {
+//     field.addEventListener(`focus`, event => {
+//         event.target.classList.remove(`error`)
+//     })
+//     field.addEventListener(`blur`, event => {
+//         if (userInputs  == "" || userInputs == " ") {
+//             event.target.classList.add(`error`)
+//         } 
+//         // else {
+//         //     event.target.classList.remove(`error`)
+//         // }
+//     })
+// })
+
+
+
+
+// function inputValidation(test) {
+//     var test = input.value.trim()
+
+// }
+
+
+// userInputs.forEach(field => {
+//     // step 4.3. blur (opposite of focus) on the field (b/c it's the target)
+//     field.addEventListener(`blur`, event => { 
+//         // step 4.2. if statement: if value === "" then add .error
+//         let userValue = inputUsername.value.trim();
+//         let passValue = inputPass.value.trim();
+//         if (userValue == "" ||  passValue == "") {
+//             // step 4.1. add .error to #input
+//             event.target.classList.add(`error`)
+//             console.log(`username is ${userValue}, password is ${passValue}`)
+//         } else {
+//             event.target.classList.remove(`error`);
+//         }
+//     }) 
+// })
+
+
+
+///////////////////////////////////////
+// THIS IS ALL YOUR SHIT FROM STEP 4 //
+///////////////////////////////////////
+
+
+
+// var userInputs = document.querySelectorAll("input")
+// var test = document.querySelector
+// test.forEach(field => {
+//     if (userInputs) {
+//         field.classList.add(`error`);
+//     }
+// })
+
+
+// if value is "", it's false
+// if else, it's true
+
+// for each of the inputs //
+
+// get value of the inputs
+// switch(true) execute some code, 
+
+
+// function > for each > blur > stuff
+// 
